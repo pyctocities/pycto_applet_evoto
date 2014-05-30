805680f57e08a070b091f40e5735ebbf8f3c3c9a
Jordi Pala <jordipalasole@gmail.com>
2014-05-30 16:12:17 +0200

    S'añade codigo

--- src/com/pycto/applet/VotoElectronicoApplet.java
+++ src/com/pycto/applet/VotoElectronicoApplet.java
@@ -1,6 +1,9 @@
 package com.pycto.applet;
 import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
@@ -16,30 +19,39 @@ import com.google.gson.Gson;
 public class VotoElectronicoApplet extends Applet {
 	
 		public CSR request_cert = new CSR();
+		public PrivateKey priv;
+		public PublicKey pub;
 	
 		@Override
 		public void start() {
 			// TODO Auto-generated method stub
 			super.start();
 			
+			setBackground(Color.BLUE);						
+			setSize(new Dimension(1024,768));
+			
+			
+			
+			
 			try {
 				//Empieza aqui: Generamos a partir de keytool de java las claves publicas y privadas
 				
-				KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+				KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
 				
 				SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
 				keyGen.initialize(1024, random);
 				
 				KeyPair pair = keyGen.generateKeyPair();
-				PrivateKey priv = pair.getPrivate();
-				PublicKey pub = pair.getPublic();
+				priv = pair.getPrivate();
+				pub = pair.getPublic();
 				
 				//Acaba aqui: Generamos a partir de keytool de java las claves publicas y privadas
 
 				//Los guardamos en la clase CSR
 				//System.out.println("La clave privada es: "+priv.getEncoded());
-				System.out.println("La clave publica es: "+pub.getEncoded());
-				
+				System.out.println("La clave publica es: "+pub);
+				System.out.println("La clave publica es: "+pub);
+
 				request_cert.setId(Integer.toString(1));
 				request_cert.setPubKey(pub.getEncoded());
 				
@@ -53,10 +65,20 @@ public class VotoElectronicoApplet extends Applet {
 				cript.reset();
 				cript.update(csr.getBytes());
 				byte[] CsrHashed = cript.digest();
-				//Aqui se tendria que hacer la modificacion del cegado...como? (linea temporal)
 				
-				byte[] CsrHashedBlinded = CsrHashed;
+				//Aqui se tendria que hacer la modificacion del cegado...como? (linea temporal)
+				BigInteger csrHashInt = new BigInteger(1, CsrHashed);
+				BigInteger m = BigInteger.valueOf(2131414141);
+				BigInteger crsHashIntBlinded = csrHashInt.mod(m);
+				System.out.println("El CSR: "+csrHashInt);
 
+				System.out.println("El CSR blinder: "+crsHashIntBlinded);
+
+				
+				
+				
+				System.out.println("CSR: "+csrHashInt);
+				//BigInteger modulus = pub.getAlgorithm().
 
 			} catch (NoSuchAlgorithmException e) {
 				// TODO Auto-generated catch block
@@ -71,8 +93,15 @@ public class VotoElectronicoApplet extends Applet {
 		public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paint(g);
-		g.drawBytes(request_cert.pubKey, 0, request_cert.pubKey.length, 10, 30);
-	    g.drawString (request_cert.pubKey.toString(), 20, 50);  
+		
+		g.setColor(Color.WHITE);
+        g.drawString("Esta a punto de votar la imagen", 400, 50);
+        
+        g.drawString(pub.toString(), 50, 80);
+
+       
+//		g.drawBytes(request_cert.pubKey, 0, request_cert.pubKey.length, 10, 30);
+//	    g.drawString (request_cert.pubKey.toString(), 20, 50);  
 
 		
 		}
