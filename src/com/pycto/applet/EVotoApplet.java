414ce593b8dd00390285ceda2aca4d2c7f3a7278
Jordi Pala <jordipalasole@gmail.com>
2014-06-20 18:43:58 +0200

    S'arregla applet

--- src/com/pycto/applet/EVotoApplet.java
+++ src/com/pycto/applet/EVotoApplet.java
@@ -1,9 +1,15 @@
 package com.pycto.applet;
 
 import java.applet.Applet;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Event;
 import java.awt.Graphics;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -11,20 +17,86 @@ import java.security.SecureRandom;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+
 import com.google.gson.Gson;
 
 public class EVotoApplet extends Applet {
 	public CSR request_cert = new CSR();
+	
+	public TextField user;
+	public TextField pass;
+	public TextField id_foto;
+    public Button okButton; 
+	public Label userlabel = new Label("Usuario: ");
+	public Label passlabel = new Label("Password: ");
+	public Label idfotolabel = new Label("Id foto: ");
 
+	
 	@Override
 	public void start() {
 		// TODO Auto-generated method stub
 		super.start();
 		
+		setBackground(Color.decode("#6D91B4"));		
+		//setSize(new Dimension(1024,400));
+		setSize(new Dimension(300,300));
 		
-		setBackground(Color.BLUE);						
-		setSize(new Dimension(1024,400));
+
+		user = new TextField(30);
+		pass = new TextField(30);
+		id_foto = new TextField(30);
+	    okButton = new Button("Votar!"); 
+
+	    
+	    userlabel.setBounds(100,85,50,20);
+	    user.setBounds(200,70,200,20);
+	    user.setVisible(true);
+	    
+	    passlabel.setBounds(100,135,50,20);
+	    pass.setBounds(200,120,200,20);
+	    pass.setEchoChar('*');
+	    
+	    idfotolabel.setBounds(100,185,50,20);
+	    id_foto.setBounds(200,170,200,20);
+	    
+	    okButton.setBounds(200,230,100,20);
+	    
+	    okButton.addActionListener(l);
+	    
+	    add(userlabel);
+	    add(user);
+	    add(passlabel);
+	    add(pass);
+	    add(idfotolabel);
+	    add(id_foto);
+	    add(okButton);
+
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+	// TODO Auto-generated method stub
+		super.paint(g);
+		g.setColor(Color.WHITE);
+	   // g.drawString("VOTACIÓN DE IMAGEN", 200, 20);
+	    
+
+	}
+    
+    public ActionListener l = new ActionListener() {
 		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+					vota();
+					JOptionPane.showMessageDialog(null, "Vota hermano!");
+				
+		}
+	};
+	
+	
+	public void vota (){
 		try {
 
 			RSAPublicKey pubKey;
@@ -99,17 +171,9 @@ public class EVotoApplet extends Applet {
 			System.out.println("ERROR: ");
 			ex.printStackTrace();
 		}
+		
 	}
-	
-	@Override
-	public void paint(Graphics g) {
-	// TODO Auto-generated method stub
-	super.paint(g);
-	
-	g.setColor(Color.WHITE);
-    g.drawString("Esta a punto de votar la imagen", 400, 50);
-    
-	}
+
 
 
 }
