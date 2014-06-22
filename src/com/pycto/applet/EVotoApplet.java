a351473d27ddc267a026ee7e03a8b7a4ac0be1de
Jordi Pala <jordipalasole@gmail.com>
2014-06-22 20:31:01 +0200

    Se ha hecho casi todo

--- src/com/pycto/applet/EVotoApplet.java
+++ src/com/pycto/applet/EVotoApplet.java
@@ -14,6 +14,7 @@ import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
@@ -26,26 +27,30 @@ import javax.swing.JPasswordField;
 import com.google.gson.Gson;
 
 public class EVotoApplet extends Applet {
+
+	public RSAPublicKey pubKey;
+	public RSAPrivateKey privKey;
 	public CSR request_cert = new CSR();
+	
 	
 	public TextField user;
 	public TextField pass;
 	public TextField id_foto;
-    public Button okButton; 
+	public Button okButton; 
 	public Label userlabel = new Label("Usuario: ");
 	public Label passlabel = new Label("Password: ");
 	public Label idfotolabel = new Label("Id foto: ");
 	public Label titol = new Label("VOTACIÓN ELECTRONICA");
 	ApiPycto api;
 
-	
+
 	@Override
 	public void start() {
 		// TODO Auto-generated method stub
 		super.start();
-		
+
 		api = new ApiPycto();
-		
+
 		setBackground(Color.decode("#6D91B4"));		
 		//setSize(new Dimension(1024,400));
 		setSize(new Dimension(450,300));
@@ -54,101 +59,188 @@ public class EVotoApplet extends Applet {
 		user = new TextField(30);
 		pass = new TextField(30);
 		id_foto = new TextField(30);
-	    okButton = new Button("Votar!"); 
-	    
-	    titol.setBounds(150,30,200,20);
-	    titol.setForeground(Color.RED);
-	    
-	    userlabel.setBounds(50,85,50,20);
-	    user.setBounds(150,85,200,20);
-	    user.setVisible(true);
-	    
-	    passlabel.setBounds(50,135,60,20);
-	    pass.setBounds(150,135,200,20);
-	    pass.setEchoChar('*');
-	    
-	    idfotolabel.setBounds(50,185,50,20);
-	    id_foto.setBounds(150,185,200,20);
-	    
-	    okButton.setBounds(150,230,100,20);
-	    okButton.addActionListener(l);
-	    
-	    add(titol);
-	    add(userlabel);
-	    add(user);
-	    add(passlabel);
-	    add(pass);
-	    add(idfotolabel);
-	    add(id_foto);
-	    add(okButton);
+		okButton = new Button("Votar!"); 
 
-	}
-	
-	@Override
-	public void paint(Graphics g) {
-	// TODO Auto-generated method stub
-		super.paint(g);
-		g.setColor(Color.WHITE);
-	   // g.drawString("VOTACIÓN DE IMAGEN", 200, 20);
-	    
+		titol.setBounds(150,30,200,20);
+		titol.setForeground(Color.RED);
 
-	}
-    
-    public ActionListener l = new ActionListener() {
+		userlabel.setBounds(50,85,50,20);
+		user.setBounds(150,85,200,20);
+		user.setVisible(true);
+
+		passlabel.setBounds(50,135,60,20);
+		pass.setBounds(150,135,200,20);
+		pass.setEchoChar('*');
+
+		idfotolabel.setBounds(50,185,50,20);
+		id_foto.setBounds(150,185,200,20);
+
+		okButton.setBounds(150,230,100,20);
+		okButton.addActionListener(l);
+
+		add(titol);
+		add(userlabel);
+		add(user);
+		add(passlabel);
+		add(pass);
+		add(idfotolabel);
+		add(id_foto);
+		add(okButton);
 		
-		@Override
-		public void actionPerformed(ActionEvent e) {
-					//vota();
-					
-					if(api.login(user.getText(), pass.getText()))
-					{
-						try {
-							String result = api.pedir_firmar_CSR_cegado(user.getText());
-							BigInteger csr_firmado = new BigInteger(result);
-							
-							JOptionPane.showMessageDialog(null, csr_firmado.toString());
-
-						} catch (NoSuchAlgorithmException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						} catch (NoSuchProviderException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						} catch (UnsupportedEncodingException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-
-					}
-					else
-					{
-						JOptionPane.showMessageDialog(null, "Usuario/Contraseña incorrectos!");
-
-					}
-
-				
-		}
-	};
-	
-	
-	public void vota (){
+		KeyPairGenerator keyGen;
 		try {
-
-			RSAPublicKey pubKey;
-			RSAPrivateKey privKey;
-
-			//generate the RSA key pair
-			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-			//initialise the KeyGenerator with a random number.
+			keyGen = KeyPairGenerator.getInstance("RSA");
 			keyGen.initialize(1024, new SecureRandom());
 			KeyPair keypair = keyGen.genKeyPair();
-			
+
 			privKey = (RSAPrivateKey)keypair.getPrivate();
 			pubKey = (RSAPublicKey)keypair.getPublic();
-			
+
 			request_cert.setId(Integer.toString(1));
 			request_cert.setPubKey(pubKey.getEncoded());
 			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//initialise the KeyGenerator with a random number.
+
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
+		g.setColor(Color.WHITE);
+		// g.drawString("VOTACIÓN DE IMAGEN", 200, 20);
+
+
+	}
+	
+	//ESte listener es para cuando se apreta el boton de votar
+
+	public ActionListener l = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//vota();
+
+			if(api.login(user.getText(), pass.getText())) //Si el login es correcto, pide la firma primero de todo
+			{
+				try {
+					String result = api.pedir_firmar_CSR_cegado(user.getText());
+					BigInteger csr_firmado = new BigInteger(result);
+					
+					JOptionPane.showMessageDialog(null, csr_firmado.toString()); //Aqui se muestra el biginteger firmado
+					
+					String fotos = id_foto.getText();
+
+					String idfotos[] = fotos.split(",");
+
+					if(idfotos.length==5){  //Estan las 5 fotos bien puestas (ejemplo: 1,2,3,4,5)
+						
+						
+						//1º - JSON de votos en claro con las ids de las fotos
+						String json_id_fotos ="[{'id':'"+idfotos[0]
+								+"'},{'id':'"+idfotos[1]
+								+"'},{'id':'"+idfotos[2]
+								+"'},{'id':'"+idfotos[3]
+								+"'},{'id':'"+idfotos[4]
+								+"'}]";
+						
+						System.out.println("\n JSON de las  5 fotos: "+json_id_fotos);
+						
+						
+						//2º- HASH de las ids de las fotos encriptado con la clave privada del usuario
+						MessageDigest md = null;
+						BigInteger HASH_json_id_fotos = null;
+						
+						try {
+							md = MessageDigest.getInstance("SHA-1");
+							md.update(json_id_fotos.getBytes());
+							byte[] passbytes = md.digest();
+							HASH_json_id_fotos = new BigInteger(1,passbytes);
+							
+						} catch (NoSuchAlgorithmException e1) {
+							e1.printStackTrace();
+						}
+						System.out.println("\n hash id foto: "+HASH_json_id_fotos);
+
+						
+						BigInteger n = pubKey.getModulus();
+						BigInteger d = privKey.getPrivateExponent();
+						
+						BigInteger hash_id_fotos_firmado = HASH_json_id_fotos.modPow(d,n);
+
+						System.out.println("\n hash_id_fotos_firmado: "+hash_id_fotos_firmado);
+						
+						
+						//3º- Pseudonimo del usuario en claro
+						
+						String pseudonimo_usuario = user.getText();
+						
+						//4º- Clave publica usuario en claro. La clave publica se divide en dos BigIntegers: 
+						//el exponente publico y el modulo.Nosotros lo separaremos por doble coma ",,"
+						
+						BigInteger exponente_public = pubKey.getPublicExponent();
+						BigInteger modulo = pubKey.getModulus();
+						
+						String exponente_publico_i_modulo = exponente_public.toString() + ".."+modulo;
+						
+						System.out.println("\n exponente_publico_i_modulo: "+exponente_publico_i_modulo);
+
+						//5º- Pseudonimo+clavepublica hasheado y firmado con la privada de la CA (es el que hemos pedido anteriormente
+						
+						String pseudonimo_clavepubli_hasheado_firmadoCA = csr_firmado.toString();
+						
+						//Formato de datos a enviar:
+						
+						String voto_enviar = 
+								json_id_fotos+"..//.."
+								+hash_id_fotos_firmado+"..//.."
+								+pseudonimo_usuario+"..//.."
+								+exponente_publico_i_modulo+"..//.."
+								+pseudonimo_clavepubli_hasheado_firmadoCA;
+						
+						System.out.println("\n Resultado a enviar: "+voto_enviar);
+
+						
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "Has de votar 5 fotos");
+					}
+
+
+
+				} catch (NoSuchAlgorithmException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (NoSuchProviderException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedEncodingException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(null, "Usuario/Contraseña incorrectos!");
+
+			}
+
+
+		}
+	};
+
+
+	public void vota (){
+		try {
+
+			//generate the RSA key pair
+
 			Gson j = new Gson();
 			String pseudonimo_sin_blindar = j.toJson(request_cert);
 
@@ -165,7 +257,7 @@ public class EVotoApplet extends Applet {
 			BigInteger n = pubKey.getModulus();
 			BigInteger gcd = null;
 			BigInteger one = new BigInteger("1");
-			
+
 			//check that gcd(r,n) = 1 && r < n && r > 1
 			do {
 				random.nextBytes(randomBytes);
@@ -206,7 +298,7 @@ public class EVotoApplet extends Applet {
 			System.out.println("ERROR: ");
 			ex.printStackTrace();
 		}
-		
+
 	}
 
 
